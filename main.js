47268d44f4df2d9f58261efc914fd464c999ee28
Magdalena <Amagdalena.C@gmail.com>
2023-11-07 12:28:05 -0500

    acabado

---
+++ main.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const app = express();
+const PORT = 80
+app.use(cors());
+app.use(express.json());
+app.listen(PORT, () => {
+    console.log(`Server runnig at port ${PORT}`);
+});
+app.use('/eco', express.static(__dirname + '/front-end'));
+
+// app.get('/eco', function(req, res) {
+//     res.sendFile(path.join(__dirname + '/front-end/index.html'));
+//   });
+  app.get('/eco/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'front-end', 'index.html'));
+});
