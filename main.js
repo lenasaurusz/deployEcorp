0601f7f2026c549a56d687b9f3c578cd4fb7be19
JessCP25 <jpayanoc25@gmail.com>
2023-11-17 18:30:59 -0500

    version v1.1.1

--- main.js
+++ main.js
@@ -5,12 +5,11 @@ const PORT = 4241
 app.listen(PORT, () => {
   console.log(`Server runnig at port ${PORT}`);
 });
-app.get('/', (req,res)=>res.send('hola'))
-app.use('/eco', express.static(__dirname + '/front-end'));
+app.use('/', express.static(__dirname + '/front-end'));
 
-app.get('/eco', function (req, res) {
+app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/front-end/index.html'));
 });
-app.get('/eco/*', function (req, res) {
+app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'front-end', 'index.html'));
 });
